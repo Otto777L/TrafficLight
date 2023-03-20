03f70816792099bde3843f373814dd8de27ba29a
Otto777L <otto.lh12@gmail.com>
2023-03-20 04:34:20 +0000

    Added change color button, and WIP of purple color

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,7 +6,19 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [color, setColor] = useState("");
+	const [color, setColor] = useState("");	
+
+	const changeColor = () => {
+		if (color == "") {
+			setColor("red");
+		} else if (color == "red") {
+			setColor("yellow");
+		} else if (color == "yellow") {
+			setColor("green");
+		} else if (color == "green") {
+			setColor("red");
+		}
+	};
 
 	return (
 		<div className="d-flex flex-column align-items-center">
@@ -17,6 +29,11 @@ const Home = () => {
 					<div onClick={()=>{setColor("red")}} className={`light m-3 red ${color=="red" ? "selected" : ""}`}></div>
 					<div onClick={()=>{setColor("yellow")}} className={`light m-3 yellow ${color=="yellow" ? "selected" : ""}`}></div>
 					<div onClick={()=>{setColor("green")}} className={`light m-3 green ${color=="green" ? "selected" : ""}`}></div>
+					<div onClick={()=>{setColor("purple")}} className={`light m-3 purple ${color=="purple" ? "selected" : ""}`}></div>
+			</div>
+			<div className="mt-3">
+				<button className="btn btn-primary" onClick={()=>{changeColor()}}>Change Color</button>
+				<button className="btn btn-secondary ms-3" onClick={()=>{showPurple()}}>Add purple Color</button>
 			</div>
 		</div>
 	);
