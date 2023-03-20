c79b1e07bdadfd262c80701dead5fd9f227e6c09
Otto777L <otto.lh12@gmail.com>
2023-03-20 04:45:13 +0000

    Added purple color

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,6 +7,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	const [color, setColor] = useState("");	
+	const [showPurple, setShowPurple] = useState(false);
 
 	const changeColor = () => {
 		if (color == "") {
@@ -15,25 +16,37 @@ const Home = () => {
 			setColor("yellow");
 		} else if (color == "yellow") {
 			setColor("green");
+		} else if (color == "green" && showPurple == true) {
+			setColor("purple");
+		} else if (color == "purple" && showPurple == true) {
+			setColor("red");
 		} else if (color == "green") {
 			setColor("red");
-		}
+		} 
 	};
 
+	const addPurple = () => {
+		setShowPurple(true);
+	}
 	return (
 		<div className="d-flex flex-column align-items-center">
 			<div className="base ">
 				
 			</div>
-			<div className="box d-flex flex-column align-items-center" >
+			{showPurple==true ? <div className="box-purple d-flex flex-column align-items-center" >
 					<div onClick={()=>{setColor("red")}} className={`light m-3 red ${color=="red" ? "selected" : ""}`}></div>
 					<div onClick={()=>{setColor("yellow")}} className={`light m-3 yellow ${color=="yellow" ? "selected" : ""}`}></div>
 					<div onClick={()=>{setColor("green")}} className={`light m-3 green ${color=="green" ? "selected" : ""}`}></div>
 					<div onClick={()=>{setColor("purple")}} className={`light m-3 purple ${color=="purple" ? "selected" : ""}`}></div>
-			</div>
+			</div> : <div className="box d-flex flex-column align-items-center" >
+					<div onClick={()=>{setColor("red")}} className={`light m-3 red ${color=="red" ? "selected" : ""}`}></div>
+					<div onClick={()=>{setColor("yellow")}} className={`light m-3 yellow ${color=="yellow" ? "selected" : ""}`}></div>
+					<div onClick={()=>{setColor("green")}} className={`light m-3 green ${color=="green" ? "selected" : ""}`}></div>
+			</div>}			
+			
 			<div className="mt-3">
 				<button className="btn btn-primary" onClick={()=>{changeColor()}}>Change Color</button>
-				<button className="btn btn-secondary ms-3" onClick={()=>{showPurple()}}>Add purple Color</button>
+				<button className="btn btn-danger ms-3" onClick={()=>{setShowPurple(true)}}>Add purple Color</button>
 			</div>
 		</div>
 	);
